29b80f510d95be3bfd3686300fcb58332ea94bad
unknown <mbogomartin25@gmail.com>
2024-08-08 11:05:43 +0300

    updated styling and frontend with backend

--- auctions/vehicles/urls.py
+++ auctions/vehicles/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('vehicle/<int:pk>/',views.vehicledetail, name='detail' ),
     path('place_bid/<int:vehicle_id>/', views.place_bid, name='place_bid'),
+    path('add-auction/', views.auction_add, name='auction_add'),
+    path('auctions/', views.auction_list, name='auction_list'),
+    path('auction/<int:pk>/', views.auction_detail, name='auction_detail'),
+    path('administration/',views.admin_dash, name='administration'),
+    path('admin/auction-status/', views.auction_status_update, name='auction_status_update'),
+
+
+
+
 ]
--- auctions/vehicles/views.py
+++ auctions/vehicles/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import get_object_or_404, render,redirect
-from .models import Vehicle, Bidding, VehicleView
+from .models import Vehicle, Bidding, VehicleView, Auction, AuctionHistory
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from .forms import BidForm
+from .forms import BidForm, AuctionForm
 from django.http.response import HttpResponseRedirect
 from django.urls import reverse
 from .filters import VehicleFilter
 from django.contrib import messages
+from .forms import AuctionForm
+from django.utils import timezone
+
 
 # Create your views here.
 def homepage(request):
@@ -40,7 +43,7 @@ def vehicledetail(request, pk):
     context = {
        'vehicle': vehicle,
        'biddings':biddings,
-       
+       'days_since_creation': vehicle.days_since_creation(),
        'similar_vehicles': similar_vehicles,       
         'highest_bid': highest_bid,
        'similar_vehicles': similar_vehicles,
@@ -57,32 +60,62 @@ def place_bid(request, vehicle_id):
         Bidding.objects.create(vehicle=vehicle, user=request.user, amount=amount)
         messages.success(request, 'Your bid has been placed successfully!')
         return HttpResponseRedirect(reverse('detail', args=[vehicle_id]))
-    
 
-# @login_required(login_url='login')
-# def Bid(request, pk):
-#     # getting post objects by their id
-#     vehicle = get_object_or_404(Vehicle, id=pk)
-#     user = request.user
-#     # form method
-#     if request.method == 'POST':
-#         form = BidForm(request.POST)
-#         # validating the form.
-#         if form.is_valid():
-#             bid = form.save(commit=False)
-#             bid.vehicle = vehicle
-#             bid.user = user
-#             bid.save()
-#             messages.success(request, 'Your bid has been placed successfully!')
-#             return HttpResponseRedirect(reverse('detail', args=[pk]))
-#     else:
-#         form = BidForm()
-#     context = {
-#         'form': form,
-#         'vehicle': vehicle,
-#     }
-    # return render(request, 'vehicles/bid.html', context)
+def auction_add(request):
+    if request.method == 'POST':
+        form = AuctionForm(request.POST)
+        if form.is_valid():
+            auction = form.save()
+            # Get the selected vehicles from the form
+            selected_vehicles = form.cleaned_data['vehicles']
+            # Update the bid_status of selected vehicles
+            for vehicle in selected_vehicles:
+                vehicle.status = 'on_auction'  # Update this status based on your needs
+                vehicle.save()
+                AuctionHistory.objects.create(
+                    vehicle=vehicle,
+                    auction=auction,
+                    start_date=auction.start_date,
+                    end_date=auction.end_date,
+                    sold=False)
+            messages.success(request, 'Auction added successfully!')
+            return redirect('auction_list')
+    else:
+        form = AuctionForm()
+    return render(request, 'admin/create_auction.html', {'form': form})   
+
+def auction_list(request):
+    auctions = Auction.objects.all()
+    return render(request, 'admin/auctions.html', {'auctions': auctions})
+
+
+def auction_detail(request, pk):
+    auction = get_object_or_404(Auction, pk=pk)
+    return render(request, 'admin/auction_details.html', {'auction': auction})
+
 @login_required
 def logout_view(request):
     logout(request)
     return redirect('homepage')
+
+# Admin views
+@login_required
+def admin_dash(request):
+    vehicles = Vehicle.objects.all()
+    auctions = Auction.objects.all()
+    auction_count = auctions.count()
+    vehicle_count = vehicles.count()
+    context = {
+        'vehicle_count': vehicle_count,
+        'auction_count': auction_count,
+    }
+    return render(request, 'admin/dashboard.html', context)
+
+
+def auction_status_update(request):
+    now = timezone.now()
+    active_auctions = Auction.objects.filter(end_date__gt=now, approved=True)
+    data = {
+        'active_auctions_count': active_auctions.count()
+    }
+    return JsonResponse(data)
